de4f9da0659d33125bbfed51de7cb3b0af1de25d
sourishBong <sourishm320@gmail.com>
2024-07-18 00:24:33 +0530

    day 2

--- Java 8/src/FindMaxElement.java
+++ Java 8/src/FindMaxElement.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class FindMaxElement {
 
     public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15);
 
+        int max=myList.stream().max(Integer::compare).get();
+        int min=myList.stream().min(Integer::compare).get();
+        System.out.println(max);
+        System.out.println(min);
     }
 }
